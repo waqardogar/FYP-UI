91ea98caa81a72cad639e7240623dd782472ce53
waqar <wqardogar5555@gmail.com>
2023-05-29 00:25:47 -0700

    up to date

--- dnda2/googlemaps/static/googlemaps/event.js
+++ dnda2/googlemaps/static/googlemaps/event.js
@@ -1,7 +1,6 @@
 
 function live_p(){
     live_preview = document.getElementById('flexSwitchCheckDefault').checked
-    console.log(live_preview)
     if(live_preview){
         document.getElementById('video-screen').style = 'background-color: rgb(114, 100, 100);position: absolute;bottom: 0;right: 0;width: 550px;height: 300px;display: flex;align-items: center;justify-content: center;' 
     }
