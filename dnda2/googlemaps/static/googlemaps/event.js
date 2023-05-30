49aec64ec09f6d603d2f7ff42e957941ec0e5e81
waqar <wqardogar5555@gmail.com>
2023-05-29 23:31:49 -0700

    video window added to top right corner

--- dnda2/googlemaps/static/googlemaps/event.js
+++ dnda2/googlemaps/static/googlemaps/event.js
@@ -1,7 +1,7 @@
 function live_p(){
     live_preview = document.getElementById('flexSwitchCheckDefault').checked
     if(live_preview){
-        document.getElementById('video-screen').style = 'position:absolute;bottom: 0;right: 0;display: flex;align-items: center;justify-content: center;' 
+        document.getElementById('video-screen').style = 'position: absolute;right: 0;display: flex;align-items: center;justify-content: center;' 
     }
     else{
         document.getElementById('video-screen').style.display = 'none' 
