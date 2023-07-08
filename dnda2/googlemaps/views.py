17676cc6edef421bac5a960a3bde39ac2f97aeaf
waqar <wqardogar5555@gmail.com>
2023-07-08 15:11:16 -0700

    Maping model deploye

--- dnda2/googlemaps/views.py
+++ dnda2/googlemaps/views.py
@@ -5,12 +5,12 @@ from django.conf import settings
 from django.http import HttpResponse
 import json
 from . models import video
-import cv2 as cv
+import cv2
 import numpy as np
 import os
-# Create your views here.
 from django.urls import reverse
 import requests
+from  .image_stitching import build_mosaic
 def dashboard(request):
     return render (request,"videostraem/video.html")
 def VideoUpload(request):
@@ -25,7 +25,7 @@ def VideoUpload(request):
         Video.save()
         def frame_extractor(path,save_dir,current_frame,gap=50):
             temp=1
-            vid = cv.VideoCapture(path)
+            vid = cv2.VideoCapture(path)
             while True:
                 flag , frame = vid.read()
                 if flag==False:
@@ -33,19 +33,40 @@ def VideoUpload(request):
                     break
                 else:
                     if(current_frame%gap==0):
-                        cv.imwrite(f"{save_dir}/{temp}.png",frame)
+                        cv2.imwrite(f"{save_dir}/{temp}.png",frame)
                         temp+=1
                 current_frame+=1
-        
         current_frame = 0
         path='.'+video_path+"/videos"
-        segments = path.split('/')  # Split the path into segments
-        new_segments = [segments[0],  segments[1],segments[-1],segments[2]]  # Rearrange the segments
+        segments = path.split('/')
+        new_segments = [segments[0],  segments[1],segments[-1],segments[2]]
         new_path = '/'.join(new_segments)
+        print(new_path)
         save_dir = os.path.join(settings.MEDIA_ROOT, 'dataset/'+area_name)
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
             save_dir = os.path.join(settings.MEDIA_ROOT, 'dataset/'+area_name)
-        frame_extractor(new_path, save_dir, current_frame,gap=50)
+        frame_extractor(new_path, save_dir, current_frame,gap=140)
+
+        #---------------------------------
+        raw_img_dir      = save_dir
+        ftr_detector     = 'sift'
+        num_keypoints    = 10000
+        save_mosaic_dir  = './media/mosaic/'
+        mosaic_base_name = area_name
+        flag_save        = True
+        #----------------------------------
+        input_images = os.listdir(raw_img_dir)
+        temp = [(n,int(i.split(".")[0])) for n,i in enumerate(input_images)]
+        temp.sort(key=lambda x:x[1])
+        input_images = [input_images[i[0]] for i in temp]
+        input_images_paths = []
+        for i in input_images:
+            name = i.split('.')[0]
+            ext = i.split('.')[1]
+            path = f"{raw_img_dir}/{name}.{ext}"
+            input_images_paths.append(path) 
+        map_path = build_mosaic(input_images_paths,save_mosaic_dir,area_name,num_keypoints,flag_save)
+        print(map_path)
         return render(request,"googlemaps/map.html",{'success': True})
     return render(request,"googlemaps/map.html",{'success': 2})
